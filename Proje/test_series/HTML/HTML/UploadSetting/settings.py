c32e95ba2acd72c08480b52ee09c62e703d3f6f7
현준 <92567978+talyer@users.noreply.github.com>
2024-12-05 22:52:51 +0900

    프론트 적용 (미리보기 없음)

--- Proje/test_series/HTML/HTML/UploadSetting/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# 업로드된 파일을 저장할 기본 경롤 설정
-MEDIA_URL = '/media' # 웹에서 접근
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # 실제 파일 저장 경로
-
-MAX_UPLOAD_SIZE = 500 * 1024 * 1024 # 10MB 파일 크기 제한
--- Proje/test_series/HTML/HTML/UploadSetting/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('upload/', views.file_upload_view, name='file_upload'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # 파일처리
